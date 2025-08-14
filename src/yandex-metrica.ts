2d965c8b132fc430bd0b2ce262d3879bcbf1b673
Dmitry Los <demonstoreby@gmail.com>
2025-08-14 22:55:48 +0300

    fix: fix ya metrica init (#47)

--- src/yandex-metrica.ts
+++ src/yandex-metrica.ts
@@ -61,9 +61,9 @@ function injectTag(options: YandexMetricaOptions): HtmlTagDescriptor[] {
 
   for (const property of properties) {
     if (property.config)
-      template += `ym(${property.id}, "init", ${JSON.stringify(property.config)});\n`
+      template += `ym("${property.id}", "init", ${JSON.stringify(property.config)});\n`
     else
-      template += `ym(${property.id}, "init");\n`
+      template += `ym("${property.id}", "init");\n`
 
     noscriptTemplate += `<img src="${NoScriptBase}${property.id}" style="position:absolute;left:-9999px;" alt="" />`
   }
